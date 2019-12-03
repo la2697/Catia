49d9dc485df775c6a63835cf5b9954db98fc1d99
la2697 <56022392+la2697@users.noreply.github.com>
2019-12-03 11:43:18 +0100

    Einführung

---
+++ Cat Übu/Cat Übu/CatiaCon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFITF;
+using MECMOD;
+using PARTITF;
+
+namespace Cat_Übu
+{
+    class CatiaCon
+    {
+        public bool CatiaLäuft()
+        {
+            object co = System.Runtime.InteropServices.Marshal.GetActiveObject("Catia.Application");
+            INFITF.Application catApp = (INFITF.Application)co;
+            return true;
+        }
+    }
+
+    
+}
---
+++ Cat Übu/Cat Übu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using INFITF;
+using MECMOD;
+using PARTITF;
+namespace Cat_Übu
+{
+    class Program
+    {
+        Program()
+        {
+            CatiaCon CK = new CatiaCon();
+            CK.CatiaLäuft();
+        }
+
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
